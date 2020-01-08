bf3b70eab57be363c739ba3653902a2eb0d3ca44
Daniel <daniel.olicruz13@gmail.com>
2020-01-07 21:47:39 -0300

    Adicionado entidades FormaPagamento e Permissao

---
+++ src/main/java/com/algaworks/algafood/domain/model/Permissao.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Permissao {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+}
