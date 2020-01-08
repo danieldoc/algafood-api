a26f39f9dae16a241f1ce2ef35e59188fcb819cf
Daniel <daniel.olicruz13@gmail.com>
2020-01-08 20:36:44 -0300

    Criado repositorio personalizado de Restaurante

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastucture/repository/RestauranteRepositoryImpl.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.infrastucture.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+        String jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
+
+        return entityManager.createQuery(jpql, Restaurante.class)
+                .setParameter("nome", "%" + nome + "%")
+                .setParameter("taxaInicial", taxaFreteInicial)
+                .setParameter("taxaFinal", taxaFreteFinal)
+                .getResultList();
+    }
+}
