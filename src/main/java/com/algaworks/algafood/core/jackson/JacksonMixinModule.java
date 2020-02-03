5499d50241f35fb4752003453c7d232e5f2f3cd9
Daniel <daniel.olicruz13@gmail.com>
2020-02-03 20:34:24 -0300

    Criado classes mixin para representacao de Cidade e Cozinha

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,6 +1,10 @@
 package com.algaworks.algafood.core.jackson;
 
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Component;
 public class JacksonMixinModule extends SimpleModule {
 
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Restaurante.class, RestauranteMixin.class)
+                .setMixInAnnotation(Cidade.class, CidadeMixin.class)
+                .setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
     }
 }
