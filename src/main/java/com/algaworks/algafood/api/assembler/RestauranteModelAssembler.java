b806435a4e0822082ae8b0c7a990144c9173175c
Daniel <daniel.olicruz13@gmail.com>
2020-02-04 19:53:30 -0300

    Adicionado ModelMapper ao projeto

--- src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
@@ -1,9 +1,9 @@
 package com.algaworks.algafood.api.assembler;
 
-import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
-import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,19 +12,11 @@ import java.util.stream.Collectors;
 @Component
 public class RestauranteModelAssembler {
 
-    public RestauranteModel toModel(Restaurante restaurante) {
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
+    @Autowired
+    private ModelMapper modelMapper;
 
-        Cozinha cozinha = restaurante.getCozinha();
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(cozinha.getId());
-        cozinhaModel.setNome(cozinha.getNome());
-        cozinhaModel.setNome(cozinha.getNome());
-        restauranteModel.setCozinha(cozinhaModel);
-        return restauranteModel;
+    public RestauranteModel toModel(Restaurante restaurante) {
+        return modelMapper.map(restaurante, RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
