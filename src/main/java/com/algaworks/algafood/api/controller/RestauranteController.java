7ea6689f282f4b0386884f88981923786c5818a0
Daniel <daniel.olicruz13@gmail.com>
2020-02-03 22:17:03 -0300

    Criado representacao RestauranteDTO

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,9 +1,12 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.exceptionhandler.ValidacaoException;
+import com.algaworks.algafood.api.model.CozinhaModel;
+import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -26,6 +29,7 @@ import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("restaurantes")
@@ -41,23 +45,31 @@ public class RestauranteController {
     private SmartValidator validator;
 
     @GetMapping
-    public List<Restaurante> listar(@RequestParam(required = false) String nome,
+    public List<RestauranteModel> listar(@RequestParam(required = false) String nome,
                                     @RequestParam(required = false) BigDecimal taxaFreteInicial,
                                     @RequestParam(required = false) BigDecimal taxaFreteFinal) {
 //        return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
-        return restauranteRepository.findAll();
+        return toCollectionModel(restauranteRepository.findAll());
+    }
+
+    private List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
+        return restaurantes.stream()
+                .map(this::toModel)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{restauranteId}")
-    public Restaurante buscar(@PathVariable Long restauranteId) {
-        return cadastroRestaurante.buscarOuFalhar(restauranteId);
+    public RestauranteModel buscar(@PathVariable Long restauranteId) {
+        Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+        return toModel(restaurante);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
+    public RestauranteModel adicionar(@RequestBody @Valid Restaurante restaurante) {
         try {
-            return cadastroRestaurante.salvar(restaurante);
+            return toModel(cadastroRestaurante.salvar(restaurante));
         } catch (
                 CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
@@ -65,20 +77,20 @@ public class RestauranteController {
     }
 
     @PutMapping("/{restauranteId}")
-    public Restaurante atualizar(@RequestBody @Valid Restaurante restaurante, @PathVariable Long restauranteId) {
+    public RestauranteModel atualizar(@RequestBody @Valid Restaurante restaurante, @PathVariable Long restauranteId) {
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
         BeanUtils.copyProperties(restaurante, restauranteAtual,
                 "id", "formasPagamento", "endereco", "dataCadastro");
         try {
-            return cadastroRestaurante.salvar(restauranteAtual);
+            return toModel(cadastroRestaurante.salvar(restauranteAtual));
         } catch (EntidadeNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
     }
 
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizarParcial(@RequestBody Map<String, Object> campos, @PathVariable Long restauranteId, HttpServletRequest request) {
+    public RestauranteModel atualizarParcial(@RequestBody Map<String, Object> campos, @PathVariable Long restauranteId, HttpServletRequest request) {
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
         merge(campos, restauranteAtual, request);
@@ -117,5 +129,20 @@ public class RestauranteController {
             Throwable rootCause = ExceptionUtils.getRootCause(e);
             throw new HttpMessageNotReadableException(e.getMessage(), rootCause, servletServerHttpRequest);
         }
+    }
+
+    private RestauranteModel toModel(Restaurante restaurante) {
+        RestauranteModel restauranteModel = new RestauranteModel();
+        restauranteModel.setId(restaurante.getId());
+        restauranteModel.setNome(restaurante.getNome());
+        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
+
+        Cozinha cozinha = restaurante.getCozinha();
+        CozinhaModel cozinhaModel = new CozinhaModel();
+        cozinhaModel.setId(cozinha.getId());
+        cozinhaModel.setNome(cozinha.getNome());
+        cozinhaModel.setNome(cozinha.getNome());
+        restauranteModel.setCozinha(cozinhaModel);
+        return restauranteModel;
     }
 }
