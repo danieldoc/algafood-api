773c94dde7eec3a93d54e5860aad483065ea8b5b
Daniel <daniel.olicruz13@gmail.com>
2020-02-06 20:04:45 -0300

    Adicionado endereco a representacao de Restaurante

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,18 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-//        ModelMapper modelMapper = new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
 //        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setTaxaFrete);
-        return new ModelMapper();
+
+        TypeMap<Endereco, EnderecoModel> enderecoModelTypeMap = modelMapper
+                .createTypeMap(Endereco.class, EnderecoModel.class);
+
+        enderecoModelTypeMap.<String>addMapping(
+                source -> source.getCidade().getEstado().getNome(),
+                (target, value) -> target.getCidade().setEstado(value));
+
+        return modelMapper;
     }
 }
