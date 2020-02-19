a3481741a73c1c837af34d2a0663897206d50b58
Daniel <daniel.olicruz13@gmail.com>
2020-02-19 20:51:11 -0300

    Adicionado paginacao no recurso de cozinhas

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -8,6 +8,10 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,8 +35,11 @@ public class CozinhaController {
     private CozinhaInputDisassembler cozinhaInputDisassembler;
 
     @GetMapping
-    public List<CozinhaModel> listar() {
-        return cozinhaModelAssembler.toCollectionModel(cozinhaRepository.findAll());
+    public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
+        Page<Cozinha> cozinhaPage = cozinhaRepository.findAll(pageable);
+        List<CozinhaModel> cozinhaModels = cozinhaModelAssembler.toCollectionModel(cozinhaPage.getContent());
+
+        return new PageImpl<>(cozinhaModels, pageable, cozinhaPage.getTotalElements());
     }
 
     @GetMapping("/{cozinhaId}")
