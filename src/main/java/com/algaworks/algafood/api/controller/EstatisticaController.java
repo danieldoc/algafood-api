51492a6b9513baf9862d70810cf67aa021f9d46e
Daniel <daniel.olicruz13@gmail.com>
2020-02-20 22:21:36 -0300

    Criado recurso para vendas diarias

---
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticaController.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.dto.VendaDiaria;
+import com.algaworks.algafood.domain.service.VendaQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("estatisticas")
+public class EstatisticaController {
+
+    @Autowired
+    private VendaQueryService vendaQueryService;
+
+    @GetMapping("vendas-diarias")
+    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filter) {
+        return vendaQueryService.consultarVendasDiarias(filter);
+    }
+}
