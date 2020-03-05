9e1b83b77f9ee23e983e00fa20444a541309dd96
Daniel <daniel.olicruz13@gmail.com>
2020-03-04 22:08:02 -0300

    Criado entidade FotoProduto

---
+++ src/main/java/com/algaworks/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+
+    @Column(nullable = false)
+    private String nomeArquivo;
+
+    private String descricao;
+
+    private String contentType;
+
+    private Long tamanho;
+}
