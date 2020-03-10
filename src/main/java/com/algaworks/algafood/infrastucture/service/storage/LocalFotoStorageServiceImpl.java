8da81b8ce52f789c9904f2ca62721dde2be3fb2d
Daniel <daniel.olicruz13@gmail.com>
2020-03-09 22:25:42 -0300

    Criado metodo recuperar arquivo

--- src/main/java/com/algaworks/algafood/infrastucture/service/storage/LocalFotoStorageServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastucture/service/storage/LocalFotoStorageServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -15,6 +16,17 @@ public class LocalFotoStorageServiceImpl implements FotoStorageService {
 
     @Value("${algafood.storage.local.diretorio-fotos}")
     private Path diretorioFotos;
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        try {
+            Path arquivoPath = getNomeArquivoPath(nomeArquivo);
+
+            return Files.newInputStream(arquivoPath);
+        } catch (IOException e) {
+            throw new StorageException("Nao foi possivel recuperar arquivo.", e);
+        }
+    }
 
     @Override
     public void armazenar(NovaFoto novaFoto) {
