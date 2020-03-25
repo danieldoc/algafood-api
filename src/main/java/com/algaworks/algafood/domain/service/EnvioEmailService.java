104c685c5dbd17ed1822ff4ca3e5ecea07e78c88
Daniel <daniel.olicruz13@gmail.com>
2020-03-25 20:59:10 -0300

    Adicionado freemarker par processar template html

--- src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -2,7 +2,10 @@ package com.algaworks.algafood.domain.service;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Singular;
 
+import java.util.Map;
 import java.util.Set;
 
 public interface EnvioEmailService {
@@ -13,8 +16,16 @@ public interface EnvioEmailService {
     @Builder
     class Mensagem {
 
+        @Singular
         private Set<String> destinatarios;
+
+        @NonNull
         private String assunto;
+
+        @NonNull
         private String corpo;
+
+        @Singular("variavel")
+        private Map<String, Object> variaveis;
     }
 }
--- src/main/java/com/algaworks/algafood/infrastucture/service/mail/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastucture/service/mail/SmtpEnvioEmailService.java
@@ -2,10 +2,13 @@ package com.algaworks.algafood.infrastucture.service.mail;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.internet.MimeMessage;
 
@@ -18,20 +21,35 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     @Autowired
     private EmailProperties emailProperties;
 
+    @Autowired
+    private Configuration freemarkerConfig;
+
     @Override
     public void enviar(Mensagem mensagem) {
         try {
+            String corpo = processarTemplate(mensagem);
+
             MimeMessage mimeMessage = mailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setFrom(emailProperties.getRemetente());
             helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
             helper.setSubject(mensagem.getAssunto());
-            helper.setText(mensagem.getCorpo(), true);
+            helper.setText(corpo, true);
 
             mailSender.send(mimeMessage);
         } catch (Exception e) {
             throw new EmailException("Nao foi possivel enviar e-mail", e);
+        }
+    }
+
+    public String processarTemplate(Mensagem mensagem) {
+        try {
+            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
+        } catch (Exception e) {
+            throw new EmailException("Nao foi montar o template do e-mail", e);
         }
     }
 }
