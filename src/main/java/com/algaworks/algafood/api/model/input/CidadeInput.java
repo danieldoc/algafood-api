a564ad30d75c23a565d403553e0ce96246522dbe
Daniel <daniel.olicruz13@gmail.com>
2020-04-23 17:44:03 -0300

    Descrito Representacoes e propriedades OpenAPI

--- src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/CidadeInput.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model.input;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class CidadeInput {
 
+    @ApiModelProperty(example = "Uberlandia")
     @NotBlank
     private String nome;
 
