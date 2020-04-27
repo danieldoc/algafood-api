978c670c7810c11706fff40e7f1ddd2b9065f186
Daniel <daniel.olicruz13@gmail.com>
2020-04-27 19:57:50 -0300

    Corrigido representacao Pageable OpenAPI

---
+++ src/main/java/com/algaworks/algafood/api/controller/openapi/model/PageableModelOpenApi.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.controller.openapi.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@ApiModel("Pageable")
+@Getter
+@Setter
+public class PageableModelOpenApi {
+
+    @ApiModelProperty(example = "0", value = "Numero da pagina (começa por 0)")
+    private int page;
+
+    @ApiModelProperty(example = "10", value = "Quantidade de elementos por pagina")
+    private int size;
+
+    @ApiModelProperty(example = "nome,asc", value = "Nome da propriedade para ordenacao")
+    private List<String> sort;
+}
