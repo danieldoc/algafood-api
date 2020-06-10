51474b557a2c274c6dd8d5ca38a4c559066d0a09
Daniel <daniel.olicruz13@gmail.com>
2020-06-09 21:59:42 -0300

    Criado documentacao usuario grupo OpenAPI

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -2,17 +2,19 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.GrupoModelAssembler;
 import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
@@ -20,18 +22,21 @@ public class UsuarioGrupoController {
     @Autowired
     private GrupoModelAssembler grupoModelAssembler;
 
+    @Override
     @GetMapping
     public List<GrupoModel> listar(@PathVariable Long usuarioId) {
         Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
         return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
     }
 
+    @Override
     @PutMapping("{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
         cadastroUsuario.associarGrupo(usuarioId, grupoId);
     }
 
+    @Override
     @DeleteMapping("{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
