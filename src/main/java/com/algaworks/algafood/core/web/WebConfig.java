e01b29dca206f70dffcc775451b7cafc5e2a7869
Daniel <daniel.olicruz13@gmail.com>
2020-08-04 16:25:27 -0300

    Configurado MediaType padrao

--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.core.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
     }
 
     @Bean
