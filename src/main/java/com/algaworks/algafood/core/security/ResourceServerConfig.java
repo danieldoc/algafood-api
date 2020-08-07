5d148addd82553300b0a43989865c9f845ecc2fd
Daniel <daniel.olicruz13@gmail.com>
2020-08-06 23:17:01 -0300

    Configurado jwt

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +24,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .cors()
             .and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec("Yy#j69bd@2ow8zMIRx4nTv!woJL!LFAj".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }
