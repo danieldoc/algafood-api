0a7f51cde46265d874138cca34e4831fc5233138
Daniel <daniel.olicruz13@gmail.com>
2020-10-20 22:12:52 -0300

    Permissao de acesso busca pedido

--- src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -10,6 +10,7 @@ import com.algaworks.algafood.api.v1.openapi.controller.PedidoControllerOpenApi;
 import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.filter.PedidoFilter;
@@ -63,6 +64,7 @@ public class PedidoController implements PedidoControllerOpenApi {
         return pedidoPagedResourcesAssembler.toModel(pedidoPage, pedidoResumoModelAssembler);
     }
 
+    @CheckSecurity.Pedidos.PodeBuscar
     @Override
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscarPorCodigo(@PathVariable String codigoPedido) {
--- src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/CheckSecurity.java
@@ -1,43 +1,56 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 public @interface CheckSecurity {
 
     @interface Cozinhas {
 
         @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.METHOD)
+        @Retention(RUNTIME)
+        @Target(METHOD)
         @interface PodeConsultar {}
 
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_COZINHAS')")
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.METHOD)
+        @Retention(RUNTIME)
+        @Target(METHOD)
         @interface PodeEditar {}
     }
 
     @interface Restaurantes {
 
         @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.METHOD)
+        @Retention(RUNTIME)
+        @Target(METHOD)
         @interface PodeConsultar {}
 
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_RESTAURANTES')")
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.METHOD)
+        @Retention(RUNTIME)
+        @Target(METHOD)
         @interface PodeGerenciarCadastro {}
 
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and hasAuthority('EDITAR_RESTAURANTES') " +
                 "or @algaSecurity.gerenciaRestaurante(#restauranteId)")
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.METHOD)
+        @Retention(RUNTIME)
+        @Target(METHOD)
         @interface PodeGerenciarFuncionamento {}
+    }
+
+    @interface Pedidos {
+
+        @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
+        @PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or "
+                + "@algaSecurity.getUsuarioId() == returnObject.cliente.id or "
+                + "@algaSecurity.gerenciaRestaurante(returnObject.restaurante.id)")
+        @Retention(RUNTIME)
+        @Target(METHOD)
+        public @interface PodeBuscar { }
     }
 }
