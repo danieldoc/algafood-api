eaff73907c7540e4c86f4e6dfa92d94aaafd8df0
Daniel <daniel.olicruz13@gmail.com>
2020-11-04 20:30:49 -0300

    Configurado auth server junto ao projeto

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +22,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .formLogin()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/oauth/**").authenticated()
+                .and()
                 .csrf().disable()
                 .cors().and()
                 .oauth2ResourceServer()
@@ -46,6 +53,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         });
 
         return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
 //    @Bean
---
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -0,0 +1,151 @@
+package com.algaworks.algafood.core.security.authorizationserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final String SCOPE_READ = "READ";
+    private static final String SCOPE_WRITE = "WRITE";
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtKeyStoreProperties jwtKeyStoreProperties;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        final int UMA_HORA_SEGUNDOS = 60 * 60;
+//        final int UM_DIA_SEGUNDOS = UMA_HORA_SEGUNDOS * 24;
+
+        clients.jdbc(dataSource);
+//                .inMemory()
+//                    .withClient("algafood-web-client")
+//                    .secret(passwordEncoder.encode("web123"))
+//                    .authorizedGrantTypes("password", "refresh_token")
+//                    .scopes(SCOPE_WRITE, SCOPE_READ)
+//                    .accessTokenValiditySeconds(UMA_HORA_SEGUNDOS * 6) // 6 horas
+//                    .refreshTokenValiditySeconds(UM_DIA_SEGUNDOS * 15) // 15 dias
+//
+////                http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalytics&redirect_uri=http://analytics.algafood.com
+//
+////                PKCE
+////                Plain
+////                http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalytics&redirect_uri=http://analytics.algafood.com&code_challenge=teste1234&code_challenge_method=plain
+////                SH256
+////                http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalytics&redirect_uri=http://analytics.algafood.com&code_challenge=teste1234&code_challenge_method=s256
+//                .and()
+//                    .withClient("foodanalytics")
+//                    .secret(passwordEncoder.encode(""))
+//                    .authorizedGrantTypes("authorization_code")
+//                    .scopes(SCOPE_WRITE, SCOPE_READ)
+//                    .redirectUris("http://analytics.algafood.com")
+//
+//                // http://localhost:8081/oauth/authorize?response_type=token&client_id=webadmin&state=abc&redirect_uri=http://web-client.com
+//                .and()
+//                    .withClient("webadmin")
+//                    .authorizedGrantTypes("implicit")
+//                    .scopes(SCOPE_WRITE, SCOPE_READ)
+//                    .redirectUris("http://web-client.com")
+//
+//                .and()
+//                    .withClient("faturamento")
+//                    .secret(passwordEncoder.encode("faturamento123"))
+//                    .authorizedGrantTypes("client_credentials")
+//                    .scopes(SCOPE_WRITE, SCOPE_READ)
+//
+//                .and()
+//                    .withClient("check-token")
+//                    .secret(passwordEncoder.encode("123web"));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//        security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("permitAll()")
+                .tokenKeyAccess("permitAll()") // libera endpoint para visualizacao da chave publica
+                .allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        var enhancerChain = new TokenEnhancerChain();
+        enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false)
+                .accessTokenConverter(jwtAccessTokenConverter())
+                .tokenEnhancer(enhancerChain)
+                .approvalStore(approvalStore(endpoints.getTokenStore()))
+                .tokenGranter(tokenGranter(endpoints));
+    }
+
+    private ApprovalStore approvalStore(TokenStore tokenStore) {
+        final var approvalStore = new TokenApprovalStore();
+        approvalStore.setTokenStore(tokenStore);
+
+        return approvalStore;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+
+//        jwtAccessTokenConverter.setSigningKey("Yy#j69bd@2ow8zMIRx4nTv!woJL!LFAj");
+
+        final var keyStorePass = jwtKeyStoreProperties.getPassword();
+        final var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
+
+        final var keyStoreKeyFactory = new KeyStoreKeyFactory(jwtKeyStoreProperties.getJksLocation(), keyStorePass.toCharArray());
+        final var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        jwtAccessTokenConverter.setKeyPair(keyPair);
+
+        return jwtAccessTokenConverter;
+    }
+
+    private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+        var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+                endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+                endpoints.getOAuth2RequestFactory());
+
+        var granters = Arrays.asList(
+                pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+        return new CompositeTokenGranter(granters);
+    }
+}
