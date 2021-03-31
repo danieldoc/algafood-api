2cdccecca665a394b434208579f4665c851dd15c
Daniel <daniel.olicruz13@gmail.com>
2021-03-31 19:06:46 -0300

    Corrigido envio email smtp

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -21,7 +21,7 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     private EmailProperties emailProperties;
 
     @Autowired
-    private Configuration freemarkerConfig;
+    private ProcessadorEmailTemplate processadorEmailTemplate;
 
     @Override
     public void enviar(Mensagem mensagem) {
@@ -34,18 +34,8 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
         }
     }
 
-    protected String processarTemplate(Mensagem mensagem) {
-        try {
-            Template template = freemarkerConfig.getTemplate(mensagem.getCorpo());
-
-            return FreeMarkerTemplateUtils.processTemplateIntoString(template, mensagem.getVariaveis());
-        } catch (Exception e) {
-            throw new EmailException("Nao foi montar o template do e-mail", e);
-        }
-    }
-
     protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
-        String corpo = processarTemplate(mensagem);
+        String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
 
